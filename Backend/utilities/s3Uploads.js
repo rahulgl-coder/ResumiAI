5ae4c347a6349c2ac49529f11019d0d736528a42
rahulgl-coder <rahulnandhu0521@gmail>
2025-08-04 18:09:08 +0530

    admin page functionalties continues

--- Backend/utilities/s3Uploads.js
+++ Backend/utilities/s3Uploads.js
@@ -1,51 +1,51 @@
-const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
-const { v4: uuidv4 } = require('uuid');
-const path = require('path');
-require('dotenv').config();
+// const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+// const { v4: uuidv4 } = require('uuid');
+// const path = require('path');
+// require('dotenv').config();
 
-const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
-
-
+// const s3Client = new S3Client({
+//   region: process.env.AWS_REGION,
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   },
+// });
 
 
 
-function generateReadableFilename(originalName, name) {
-  const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
-  const cleanName = name.replace(/\s+/g, '_').replace(/[^a-zA-Z0-9_]/g, '');
-  const ext = path.extname(originalName);
-  return `resume_${cleanName}_${timestamp}${ext}`;
-}
 
-const uploadToS3 = async (fileBuffer, originalName, name) => {
-  const Key = generateReadableFilename(originalName, name);
 
-  const uploadParams = {
-    Bucket:process.env.AWS_BUCKET_NAME,
-    Key,
-    Body:fileBuffer,
-    ContentType: 'application/pdf',
-    ACL: 'public-read',
-  };
+// function generateReadableFilename(originalName, name) {
+//   const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+//   const cleanName = name.replace(/\s+/g, '_').replace(/[^a-zA-Z0-9_]/g, '');
+//   const ext = path.extname(originalName);
+//   return `resume_${cleanName}_${timestamp}${ext}`;
+// }
 
-  const command = new PutObjectCommand(uploadParams);
+// const uploadToS3 = async (fileBuffer, originalName, name) => {
+//   const Key = generateReadableFilename(originalName, name);
 
-  try {
-    await s3Client.send(command);
+//   const uploadParams = {
+//     Bucket:process.env.AWS_BUCKET_NAME,
+//     Key,
+//     Body:fileBuffer,
+//     ContentType: 'application/pdf',
+//     ACL: 'public-read',
+//   };
 
-    return {
-      Location: `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${Key}`,
-      Key,
-    };
-  } catch (error) {
-    console.error('S3 upload error:', error);
-    throw error;
-  }
-};
+//   const command = new PutObjectCommand(uploadParams);
 
-module.exports = { uploadToS3 };
+//   try {
+//     await s3Client.send(command);
+
+//     return {
+//       Location: `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${Key}`,
+//       Key,
+//     };
+//   } catch (error) {
+//     console.error('S3 upload error:', error);
+//     throw error;
+//   }
+// };
+
+// module.exports = { uploadToS3 };
